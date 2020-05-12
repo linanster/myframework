2b9bd4fb317ac29af400034588d3eefc44be7fa1
LiNan <believelinan@aliyun.com>
2020-05-12 19:50:34 +0800

    update mysettings.py/SECRET_KEY

--- app/mysettings.py
+++ app/mysettings.py
@@ -1,4 +1,4 @@
-SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+SECRET_KEY = "youdonotknowme"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/ge' 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
