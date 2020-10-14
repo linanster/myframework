c07ba6e513d14c3fceeaf49b9c92be525cf58f97
LiNan <believelinan@aliyun.com>
2020-10-14 14:52:08 +0800

    implement login next_page [*]

--- app/views/blue_auth.py
+++ app/views/blue_auth.py
@@ -14,16 +14,29 @@ blue_auth = Blueprint('blue_auth', __name__, url_prefix='/auth')
 @viewfunclog
 def login():
     logger_module1.warn('client login from {}'.format(request.remote_addr))
+    # 1. GET method, 进入登录页面
     if request.method == 'GET':
-        return render_template('auth_login.html')
+        # 1.1 http://10.30.30.101:4000/auth/login, next_page为None
+        # 1.2 http://10.30.30.101:4000/auth/login?next=%2Fadmin%2Findex, next_page为/admin/index
+        next_page = request.args.get('next')
+        return render_template('auth_login.html', next_page=next_page)
+    # 2. Post method, 处理登录请求
     username = request.form.get('username')
     password = request.form.get('password')
+    # 2.1 login表单没有next参数时，next_page为None
+    # 2.2 login表单有next参数时，next_page为对应字符串，如/admin/index
+    next_page = request.form.get('next')
     # user = User.query.filter_by(username=username, password=password).first()
     user = User.query.filter_by(username=username).first()
     if not user or not user.verify_password(password):
         return render_template('auth_login.html', warning="login failed!")
+    login_user(user)
+    # 当next_page不为空时，重定向至next_page
+    # next_page 形如 /admin/index
+    if next_page:
+        return redirect(next_page)
+    # 当next_page为空时，重定向至主页
     else:
-        login_user(user)
         return redirect(url_for('blue_main.vf_index'))
 
 @blue_auth.route('/logout')
